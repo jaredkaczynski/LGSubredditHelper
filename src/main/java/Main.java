44dda385ae826b12b2d69f1abb2db1cc39c101f4
Jared <razrsword@gmail.com>
2015-05-23 01:59:36 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by razrs on 5/23/2015.
+ */
+class Main {
+    public static void main(String[] args) {
+       while(){
+
+       }
+    }
+    void isUpdateTime(){
+
+    }
+
+}
---
+++ src/main/java/TextEditor.java
@@ -0,0 +1,5 @@
+/**
+ * Created by razrs on 5/23/2015.
+ */
+public class TextEditor {
+}
