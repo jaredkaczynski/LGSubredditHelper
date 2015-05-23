4a27ec5997283a5895b5909a06106ca7d46bcb77
Jared <razrsword@gmail.com>
2015-05-23 11:48:32 -0400

    XML Scraper grabs the post ID successfully

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,14 @@
  */
 class Main {
     public static void main(String[] args) {
-       while(){
-
-       }
+        //String url = args[0];
+        String subreddit = "lgg2";
+        XMLScraper test = new XMLScraper();
+        test.grabContestURL(subreddit);
+        test.grabTopPosterInfo(subreddit);
     }
-    void isUpdateTime(){
+
+    void isUpdateTime() {
 
     }
 
--- src/main/java/XMLScraper.java
+++ src/main/java/XMLScraper.java
@@ -1,4 +1,20 @@
+import com.github.jreddit.entity.Comment;
+import com.github.jreddit.entity.Submission;
+import com.github.jreddit.entity.User;
+import com.github.jreddit.exception.RedditError;
+import com.github.jreddit.exception.RetrievalFailedException;
+import com.github.jreddit.retrieval.Comments;
+import com.github.jreddit.retrieval.ExtendedComments;
+import com.github.jreddit.retrieval.ExtendedSubmissions;
+import com.github.jreddit.retrieval.Submissions;
+import com.github.jreddit.retrieval.params.*;
+import com.github.jreddit.utils.RedditConstants;
+import com.github.jreddit.utils.restclient.HttpRestClient;
+import com.github.jreddit.utils.restclient.RestClient;
+import examples.Authentication;
 import jdk.internal.org.xml.sax.SAXException;
+import org.json.simple.parser.ParseException;
+import org.jsoup.Jsoup;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -9,52 +25,147 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.List;
 
 /**
- * Created by razrs on 5/23/2015.
+ * This class should do all XML Scraping work
+ * Things like returning the top post information
+ * and whether it is time to update the images or not
  */
 public class XMLScraper {
 
-
-    private void parseXmlFile(){
+    /**
+     * @param subreddit
+     * @return This grabs the top post from the subreddit which should
+     * be the modpost if the automoderator is working properly
+     */
+    public String grabContestURL(String subreddit) {
+        String modPostID = null;
+        /*
         try {
-            
-            File fXmlFile = new File("/Users/mkyong/staff.xml");
+            String html = Jsoup.connect(url).get().html();
+            File fXmlFile = new File(html);
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(fXmlFile);
 
-            //optional, but recommended
-            //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
             doc.getDocumentElement().normalize();
 
-            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+            NodeList items = doc.getElementsByTagName("item");
 
-            NodeList nList = doc.getElementsByTagName("staff");
+            NodeList postInfo = items.item(0).getChildNodes();
 
-            System.out.println("----------------------------");
+            modPostURL = postInfo.item(1).getNodeValue();
 
-            for (int temp = 0; temp < nList.getLength(); temp++) {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+        RestClient restClient = new HttpRestClient();
+        restClient.setUserAgent("bot/1.0 by name");
 
-                Node nNode = nList.item(temp);
+        // Connect the user
+        User user = new User(restClient, Authentication.getUsername(), Authentication.getPassword());
+        try {
+            user.connect();
+        } catch (IOException e1) {
+            System.err.println("I/O Exception occured when attempting to connect user.");
+            e1.printStackTrace();
+            //return;
+        } catch (ParseException e1) {
+            System.err.println("I/O Exception occured when attempting to connect user.");
+            e1.printStackTrace();
+            //return;
+        }
 
-                System.out.println("\nCurrent Element :" + nNode.getNodeName());
+        try {
 
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+            /***************************************************************************************************
+             * First: basic API functionality
+             */
 
-                    Element eElement = (Element) nNode;
+            // Handle to Submissions, which offers the basic API functionality
+            Submissions subms = new Submissions(restClient, user);
 
-                    System.out.println("Staff id : " + eElement.getAttribute("id"));
-                    System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
-                    System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
-                    System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
-                    System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
+            // Retrieve submissions of a submission
+            System.out.println("\n============== Basic subreddit submissions ==============");
+            List<Submission> submissionsSubreddit = subms.ofSubreddit(subreddit, SubmissionSort.HOT, 1, 10, null, null, false);
 
-                }
-            }
+            //subms.of
+            //printSubmissionsList(submissionsSubreddit);
+            if (submissionsSubreddit.get(0).getTitle().contains("Photography and Homescreen")) {
+                //System.out.println(submissionsSubreddit.get(0).getIdentifier());
+                return (submissionsSubreddit.get(0).getIdentifier());
+            } else
+                return ("Error");
+
+        } catch (RetrievalFailedException e) {
+            e.printStackTrace();
+        } catch (RedditError e) {
+            e.printStackTrace();
+        }
+
+        return (modPostID);
+    }
+
+    /*
+    public Array grabTopPosterInfo(String url){
+        try {
+            String html = Jsoup.connect(grabContestURL(url)).get().html();
+            File fXmlFile = new File(html);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+
+            doc.getDocumentElement().normalize();
+
+            NodeList items = doc.getElementsByTagName("item");
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
+        return();
+    }*/
+    public Comments grabTopPosterInfo(String url) {
+        RestClient restClient = new HttpRestClient();
+        restClient.setUserAgent("bot/1.0 by name");
+        if(url == "Error"){
+            return(null);
+        }
+        // Connect the user
+        User user = new User(restClient, Authentication.getUsername(), Authentication.getPassword());
+        try {
+            user.connect();
+        } catch (IOException e1) {
+            System.err.println("I/O Exception occured when attempting to connect user.");
+            e1.printStackTrace();
+            //return;
+        } catch (ParseException e1) {
+            System.err.println("I/O Exception occured when attempting to connect user.");
+            e1.printStackTrace();
+            //return;
+        }
+
+        try {
+
+            /***************************************************************************************************
+             * First: basic API functionality
+             */
+
+            // Handle to Comments, which offers the basic API functionality
+            Comments coms = new Comments(restClient, user);
+
+            // Retrieve comments of a submission
+            System.out.println("\n============== Basic submission comments ==============");
+            List<Comment> commentsSubmission = coms.ofSubmission(url, null, 0, 8, 100, CommentSort.TOP);
+            Comments.printCommentTree(commentsSubmission);
+
+        } catch (RetrievalFailedException e) {
+            e.printStackTrace();
+        } catch (RedditError e) {
+            e.printStackTrace();
+        }
+        return (null);
     }
 }
